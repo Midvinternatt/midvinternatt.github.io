0d60160dc664370dc97c5165979c5af634b1cd5f
Midvinternatt <43345799+Midvinternatt@users.noreply.github.com>
2023-11-10 01:02:11 +0100

    Kod som rör gränssnittet flyttat till separat fil

--- script.js
+++ script.js
@@ -1,53 +1,15 @@
 console.log("Running script.js");
 
-LoadGameData();
 
-function OnSearchInput() {
-    UpdateGameList();
+
+async function Initialize() {
+    document.getElementById("searchfield-input").value = "";
+    document.getElementById("player-input").value = "";
+
+    await LoadGameData();
+
+    PopulateGenreList();
+    PopulateGameList();
 }
 
-function UpdateGameList() {
-    document.querySelectorAll(".game").forEach(gameDiv => {
-        gameDiv.style.display = "none";
-    });
-
-    console.log(document.getElementById("searchfield-input").value);
-
-    let filteredGames = GetAllGames()
-    filteredGames = FilterGamesMatching(GetAllGames(), document.getElementById("searchfield-input").value);
-
-    filteredGames.forEach(game => {
-        game.div.style.display = "block";
-    });
-}
-
-// function FillGenreList() {
-//     console.log(genreList);
-//     genreList.forEach(genre => {
-//         console.log(genre);
-//     });
-// }
-
-
-// let a = {"a": 1};
-// let b = {"b": a};
-// let c = {"c": a};
-
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log("-");
-
-// console.log(b.b==c.c);
-
-// console.log(a.a);
-// console.log(b.b.a);
-// console.log(c.c.a);
-// console.log("-");
-
-// b.b.a = 5;
-
-// console.log(a.a);
-// console.log(b.b.a);
-// console.log(c.c.a);
-// console.log("-");
+Initialize();
