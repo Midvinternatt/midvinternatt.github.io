dc4a10faaa38c38360ec028afc77a0400f9a0b5f
Midvinternatt <43345799+Midvinternatt@users.noreply.github.com>
2025-04-13 22:23:18 +0200

    Added Sprite-class

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,22 +1,44 @@
-export var SPRITE_TYPE;
-(function (SPRITE_TYPE) {
-    SPRITE_TYPE["PLAYER_SHIP"] = "ship.png";
-})(SPRITE_TYPE || (SPRITE_TYPE = {}));
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+export var SPRITE;
+(function (SPRITE) {
+    SPRITE[SPRITE["PLAYER_SHIP"] = 0] = "PLAYER_SHIP";
+    SPRITE[SPRITE["DRONE"] = 1] = "DRONE";
+})(SPRITE || (SPRITE = {}));
+const spriteSheet = [
+    { id: SPRITE.PLAYER_SHIP, file: "ship.png", w: 100, h: 100 },
+    { id: SPRITE.DRONE, file: "1.png", w: 100, h: 100 }
+];
 class Sprite {
+    constructor(bitmap) {
+        this.bitmap = bitmap;
+    }
     static LoadSprites() {
-        let el = document.getElementById("ship");
-        createImageBitmap(el).then((bitmap) => {
-            this.imageBitmapList.set(SPRITE_TYPE.PLAYER_SHIP, bitmap);
+        return __awaiter(this, void 0, void 0, function* () {
+            const promises = spriteSheet.map((sprite) => __awaiter(this, void 0, void 0, function* () {
+                var _a, _b;
+                const img = new Image();
+                img.src = sprite.file;
+                const bitmap = yield createImageBitmap(img, (_a = sprite.x) !== null && _a !== void 0 ? _a : 0, (_b = sprite.y) !== null && _b !== void 0 ? _b : 0, sprite.w, sprite.h);
+                return ({ sprite, bitmap });
+            }));
+            const resolves = yield Promise.all(promises);
+            resolves.forEach((obj) => {
+                Sprite.spriteMap.set(obj.sprite.id, new Sprite(obj.bitmap));
+            });
         });
-        // this.imageBitmapList.set(SPRITE.PLAYER_SHIP, value);
-        // let p = new Promise(() => {
-        //     let el = <HTMLImageElement> document.getElementById("ship");
-        //     return createImageBitmap(el);
-        // }).then((value) => {
-        //     // this.imageBitmapList.set(SPRITE.PLAYER_SHIP, value);
-        // });
+    }
+    static getSprite(sprite) {
+        return this.spriteMap.get(sprite);
     }
 }
-Sprite.imageBitmapList = new Map();
+Sprite.spriteMap = new Map();
 export default Sprite;
 //# sourceMappingURL=Sprite.js.map
--- ts/Sprite.ts
+++ ts/Sprite.ts
@@ -1,14 +1,110 @@
-export enum SPRITE_TYPE {
-    PLAYER_SHIP = "ship.png"
+export enum SPRITE {
+    PLAYER_SHIP,
+    DRONE
 }
-export default class Sprite {
-    static imageBitmapList: Map<SPRITE_TYPE, ImageBitmap> = new Map();
 
-    static LoadSprites() {
-        let el = <HTMLImageElement> document.getElementById("ship");
-        createImageBitmap(el).then((bitmap) => {
-            this.imageBitmapList.set(SPRITE_TYPE.PLAYER_SHIP, bitmap);
+type spritedata = {
+    id: SPRITE,
+    file: string,
+    x?: number,
+    y?: number,
+    w: number,
+    h: number
+}
+
+const spriteSheet: spritedata[] = [
+    { id: SPRITE.PLAYER_SHIP, file: "ship.png", w: 100, h: 100 },
+    { id: SPRITE.DRONE, file: "1.png", w: 100, h: 100 }
+];
+
+export default class Sprite {
+    static spriteMap: Map<SPRITE, Sprite> = new Map();
+
+    constructor(public bitmap: ImageBitmap) { }
+
+    static async LoadSprites(): Promise<void> {
+        const promises = spriteSheet.map(async (sprite: spritedata) => {
+            const img = new Image();
+            img.src = sprite.file;
+            const bitmap = await createImageBitmap(img, sprite.x ?? 0, sprite.y ?? 0, sprite.w, sprite.h);
+            return ({ sprite, bitmap });
         });
+
+        const resolves = await Promise.all(promises);
+        resolves.forEach((obj) => {
+            Sprite.spriteMap.set(obj.sprite.id, new Sprite(obj.bitmap));
+        });
+    }
+    static getSprite(sprite: SPRITE): Sprite {
+        return this.spriteMap.get(sprite);
+    }
+    // static createSprite(elementId: string, index: SPRITE): Promise<Sprite | void> {
+    //     return Promise.resolve(
+    //         createImageBitmap(<HTMLImageElement> document.getElementById(elementId))
+    //     ).then((bitmap) => {
+    //         console.log("createSprite", index, bitmap);
+    //         let sprite = new Sprite(bitmap);
+    //         Sprite.spriteSheet.set(index, sprite);
+    //         return sprite;
+    //     }, () => {});
+    // }
+    // static LoadSprites() {
+    //     const arr = [
+    //         createImageBitmap(<HTMLImageElement> document.getElementById("1")),
+    //         createImageBitmap(<HTMLImageElement> document.getElementById("2")),
+    //         createImageBitmap(<HTMLImageElement> document.getElementById("3")),
+    //         createImageBitmap(<HTMLImageElement> document.getElementById("PLAYER_SHIP")),
+    //         Sprite.createSprite("PLAYER_SHIP", SPRITE.PLAYER_SHIP)
+    //      ];
+
+    //     return Promise.all(arr).then((resolves) => {
+    //         console.log("LoadSprites", resolves);
+    //     });
+/*
+        Object.keys(SPRITE).filter((key) => {
+            console.log(SPRITE[key]);
+            const blob = await fetch(SPRITE[key]).then(resp => resp.ok && resp.blob());
+        });
+
+        this.inload = 0;
+        document.getElementById("sprites").querySelectorAll("img").forEach((element: HTMLImageElement) => {
+            console.log(element.id);
+            this.inload++;
+            this.tot++;
+            createImageBitmap(element).then((bitmap) => {
+                this.imageBitmapList.set(SPRITE.PLAYER_SHIP, bitmap);
+                this.inload--;
+
+                if(this.inload==0)
+                    console.log("done " + this.tot);
+                    
+            });
+        });
+
+        Object.keys(SPRITE).forEach(key => {
+            const blob = await fetch(SPRITE[key]).then(resp => resp.ok && resp.blob());
+        });
+
+        async function loadSprites() {
+            const keys = Object.keys(Images);
+            for(let i = 0; i < keys.length; i++) {
+              const key = keys[i];
+              // fetch as Blob
+              const blob = await fetch(Spritesheets[key]).then(resp => resp.ok && resp.blob());
+              const img = Images[key];
+          
+              for(let k = 0; k < img.width / img.spriteW; k++) {
+                img[k] = await createImageBitmap(blob, k * img.spriteW, 0, img.spriteW, img.spriteH);
+              };
+            };
+            console.log(Images)
+          };
+*/
+
+        // let el = <HTMLImageElement> document.getElementById("ship");
+        // createImageBitmap(el).then((bitmap) => {
+        //     this.imageBitmapList.set(SPRITE.PLAYER_SHIP, bitmap);
+        // });
         // this.imageBitmapList.set(SPRITE.PLAYER_SHIP, value);
 
         // let p = new Promise(() => {
@@ -17,5 +113,5 @@ export default class Sprite {
         // }).then((value) => {
         //     // this.imageBitmapList.set(SPRITE.PLAYER_SHIP, value);
         // });
-    }
+    // }
 }
